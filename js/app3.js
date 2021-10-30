555e9b35dbd732ad2ab99325e1826b9ca996c386
pcela <pcela@o2.pl>
2021-10-30 23:33:16 +0200

    Update app3.js

--- js/app3.js
+++ js/app3.js
@@ -1256,12 +1256,12 @@ function utc2delhi(utc_datetime) {
 
 // bytes adaptive conversion to KB, MB, GB
 function formatFileSize(bytes) {
-    if (bytes >= 1000000000) {
-        bytes = (bytes / 1000000000).toFixed(2) + ' GB';
-    } else if (bytes >= 1000000) {
-        bytes = (bytes / 1000000).toFixed(2) + ' MB';
-    } else if (bytes >= 1000) {
-        bytes = (bytes / 1000).toFixed(2) + ' KB';
+    if (bytes >= 1073741824) {
+        bytes = (bytes / 1073741824).toFixed(2) + ' GB';
+    } else if (bytes >= 1048576) {
+        bytes = (bytes / 1048576).toFixed(2) + ' MB';
+    } else if (bytes >= 1024) {
+        bytes = (bytes / 1024).toFixed(2) + ' KB';
     } else if (bytes > 1) {
         bytes = bytes + ' bytes';
     } else if (bytes == 1) {
