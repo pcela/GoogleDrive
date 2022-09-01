0f024c98b7fd7df1a34a60cfc1e109ad25ae9761
pcela <15870910+pcela@users.noreply.github.com>
2022-09-01 12:18:25 +0200

    Update app-multiple-drives1.js

--- js/app-multiple-drives1.js
+++ js/app-multiple-drives1.js
@@ -184,6 +184,7 @@ function nav(path) {
 	    <a class="nav-link" href="${UI.link_anidb}" target="_blank">${UI.nav_link_6}</a>
 	    <li class="nav-item">
 	    <a class="nav-link" href="${UI.link_chomikuj}" target="_blank">${UI.nav_link_7}</a>
+	    </li>`;    
   </li>${UI.show_logout_button ?'<li class="nav-item"><a class="nav-link" href="/logout">Logout</a></li>': ''}`;
 
     var search_text = model.is_search_page ? (model.q || '') : '';
